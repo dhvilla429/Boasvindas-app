f9bdd728f77db1d849bb49760d24a478b0e8ba3f
dhvilla429 <dhvillamap@gmail.com>
2026-05-28 15:27:04 -0300

    Add files via upload

---
+++ src/data.ts
@@ -0,0 +1,7 @@
+import { SurveySubmission } from "./types";
+
+export const INITIAL_PRODUCTS: string[] = [];
+
+export const INITIAL_LEADERS: string[] = [];
+
+export const INITIAL_SUBMISSIONS: SurveySubmission[] = [];
