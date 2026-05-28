610f35cb15040201a6133cd549f10e9d62b2e097
dhvilla429 <dhvillamap@gmail.com>
2026-05-28 14:17:48 -0300

    Delete deploy_novo directory

--- deploy_novo/src/data.ts
+++
@@ -1,7 +0,0 @@
-import { SurveySubmission } from "./types";
-
-export const INITIAL_PRODUCTS: string[] = [];
-
-export const INITIAL_LEADERS: string[] = [];
-
-export const INITIAL_SUBMISSIONS: SurveySubmission[] = [];
