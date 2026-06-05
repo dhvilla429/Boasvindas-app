fa08cd550ac4de436c223866e78ed0bccb7e9cbe
dhvilla429 <dhvillamap@gmail.com>
2026-06-05 14:24:25 -0300

    fix vite config completo

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+});
